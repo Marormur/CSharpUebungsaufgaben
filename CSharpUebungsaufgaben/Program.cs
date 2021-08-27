a49c07b73d81d9cbc6c0c90ecfb76fe94287b57c
marvin <marvintemmen@gmail.com>
2021-08-27 09:03:34 +0200

    * .gitignore: asdas
    * CSharpUebungsaufgaben.sln:
    * Program.cs:
    * CSharpUebungsaufgaben.csproj:

---
+++ CSharpUebungsaufgaben/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpUebungsaufgaben
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Worldaaaaaaaaaaa!");
+        }
+    }
+}
